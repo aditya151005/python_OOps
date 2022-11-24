9a495caef6f5e3e67c234df71919ef399d137567
Sonu <adityasonu1211998@gmail.com>
2022-11-24 09:10:32 +0530

    initial committed

---
+++ 1.oops_basics.py
@@ -0,0 +1,8 @@
+# python supports different programming paradigm-
+# 1.procedure oriented programming(different modules will be a function)
+# 2.functional programming
+# 3.oop
+# object is an instance of the class.
+# object has an attributes(variables) and behaviour(function/methods)
+# class is the blueprint of the object.
+# creating the class-
