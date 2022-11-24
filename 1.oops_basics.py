d86fa259e826f78a62c8034630166b7e02a40fb2
Sonu <adityasonu1211998@gmail.com>
2022-11-24 09:20:11 +0530

    initial committed

--- 1.oops_basics.py
+++ 1.oops_basics.py
@@ -6,3 +6,13 @@
 # object has an attributes(variables) and behaviour(function/methods)
 # class is the blueprint of the object.
 # creating the class-
+class Computer:
+    def config(self):
+        print("i5,16gb,1TB")
+com1=Computer()
+com2=Computer()
+print(type(com1)) #<class '__main__.Computer'>
+Computer.config(com1) #i5,16gb,1TB
+Computer.config(com2) #i5,16gb,1TB
+com1.config()  #i5,16gb,1TB
+com2.config()   #i5,16gb,1TB
